362ca66d1f826e0fe1b6d143aaa7b4db1d55c477
Kevin Rasmusson <kevin.rasmusson@hotmail.com>
2021-06-29 21:18:35 +0200

    Started building http service.

--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -1,9 +1,45 @@
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type':  'application/json'
+  })
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpService {
 
-  constructor() { }
+  private configUrl: string;
+
+  constructor(private http: HttpClient) {
+    this.configUrl = "/api/";
+  }
+
+  public getBankInfo(): Observable<any> {
+    return this.http.post(this.configUrl + '', httpOptions).pipe(
+      catchError(this.handleError)
+    )
+  }
+
+  // Error handler
+  private handleError(error: HttpErrorResponse) {
+    if (error.status === 0) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong.
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${error.error}`);
+    }
+    // Return an observable with a user-facing error message.
+    return throwError(
+      'Something bad happened; please try again later.');
+  }
 }
